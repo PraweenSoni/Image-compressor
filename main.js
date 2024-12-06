8cad37ddee64c82de0e136f722c27bd1ed4193e1
Praween Soni <pramrajkumar1234@gmail.com>
2024-12-06 22:37:22 +0530

    Bugs fixed on seleting image no load first time

--- main.js
+++ main.js
@@ -1,55 +1,58 @@
-const img_box = document.querySelector('.img_box'),
-previewimg = img_box.querySelector("img"),
-fileInput = img_box.querySelector("input"),
-widthinput = document.querySelector(".size #width"),
-heightinput = document.querySelector(".size #height"),
-radioinput = document.querySelector(".option  #resize"),
-quality = document.querySelector(".option #reduce"),
-downloadbtn = document.querySelector("#downloadbtn");
+const img_box = document.querySelector(".img_box"),
+  previewimg = img_box.querySelector("img"),
+  fileInput = img_box.querySelector("input"),
+  widthinput = document.querySelector(".size #width"),
+  heightinput = document.querySelector(".size #height"),
+  radioinput = document.querySelector(".option  #resize"),
+  quality = document.querySelector(".option #reduce"),
+  downloadbtn = document.querySelector("#downloadbtn");
 let ogimgratio;
-const loadfile = (e) => {
- const file = e.target.files[0];
- if (!file) return;
- previewimg.src = URL.createObjectURL(file);
- previewimg.addEventListener("load",()=>{
-  widthinput.value = previewimg.naturalWidth;
-  heightinput.value = previewimg.naturalHeight;
-  ogimgratio = previewimg.naturalWidth / previewimg.naturalHeight;
- });
-}
 
-widthinput.addEventListener("keyup",()=>{
- const height = radioinput.checked ? widthinput.value / ogimgratio : heightinput.value;
+widthinput.addEventListener("keyup", () => {
+  const height = radioinput.checked
+    ? widthinput.value / ogimgratio
+    : heightinput.value;
   heightinput.value = Math.floor(height);
-})
-heightinput.addEventListener("keyup",()=>{
- const width = radioinput.checked ? heightinput.value * ogimgratio : widthinput.value;
+});
+heightinput.addEventListener("keyup", () => {
+  const width = radioinput.checked
+    ? heightinput.value * ogimgratio
+    : widthinput.value;
   widthinput.value = Math.floor(width);
-})
+});
 
-const resizeAnddownload = ()=>{
+const resizeAnddownload = () => {
+  const canvas = document.createElement("canvas");
+  const a = document.createElement("a");
 
- const canvas = document.createElement("canvas");
- const a = document.createElement("a");
+  const ctx = canvas.getContext("2d");
 
- const ctx = canvas.getContext("2d");
- 
- //also pass 0.1 : 1.0  for more reduce quality
- const imgQuality = quality.checked ? 0.7 : 1.0;
- 
- canvas.width = widthinput.value;
- canvas.height = heightinput.value;
- 
- ctx.drawImage(previewimg, 0, 0,canvas.width, canvas.height);
- //document.body.appendChild(canvas);
- a.href = canvas.toDataURL("image/jpeg", imgQuality);
- a.download = new Date().getTime();
- a.click();
-}
+  //also pass 0.1 : 1.0  for more reduce quality (0.1 is lowest)
+
+  const imgQuality = quality.checked ? 0.1 : 1.0;
+
+  canvas.width = widthinput.value;
+  canvas.height = heightinput.value;
+
+  ctx.drawImage(previewimg, 0, 0, canvas.width, canvas.height);
+  //document.body.appendChild(canvas);
+  a.href = canvas.toDataURL("image/jpeg", imgQuality);
+  a.download = new Date().getTime();
+  a.click();
+};
 
 downloadbtn.addEventListener("click", resizeAnddownload);
 
-fileInput.addEventListener("click", loadfile);
-img_box.addEventListener("click",()=>{
- fileInput.click();
-})
+fileInput.onchange = (e) =>{
+  const file = e.target.files[0];
+  if (!file) return;
+  previewimg.src = URL.createObjectURL(file);
+  previewimg.addEventListener("load", () => {
+    widthinput.value = previewimg.naturalWidth;
+    heightinput.value = previewimg.naturalHeight;
+    ogimgratio = previewimg.naturalWidth / previewimg.naturalHeight;
+  });
+}
+img_box.addEventListener("click", () => {
+  fileInput.click();
+});
